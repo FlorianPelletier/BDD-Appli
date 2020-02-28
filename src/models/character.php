2db62fa9797ce050fca646401adb6ecde4ff3500
paullouppe <paullouppe@gmail.com>
2020-02-28 11:13:21 +0100

    remove empty models implements game

--- src/models/character.php
+++ src/models/character.php
@@ -8,5 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class character extends Model
 {
+    //nom de la table dans la BD
+    protected $table = "character";
+    //clé primaire dans la BD
+    protected $primaryKey = "id";
 
+    //association des clés étrangères
+    public function asso(){
+        return $this->hasMany('src\models\character', '');
+    }
 }
--- src/models/game.php
+++ src/models/game.php
@@ -8,5 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class game extends Model
 {
+    protected $table = 'game';
+    protected $primaryKey = 'id';
 
+    public function appears_in(){
+        return $this->belongsToMany('Model\character', 'game2character','game_id', 'character_id');
+    }
 }
