1cbc8ef2c7421e691477aeebe44e1332696a6a8c
paullouppe <paullouppe@gmail.com>
2020-03-24 08:57:30 +0100

    séance 5 préparation

--- index.php
+++ index.php
@@ -13,75 +13,12 @@ require_once 'vendor/autoload.php';
 $app = new Slim();
 
 $app->get('/', function () {
-    $vueIndex = new VueAccueil();
-    $vueIndex->render(1);
-
-
-    $controleur = new src\controleurs\controleurquestion();
-    ///------Partie 1 -----///
-
-    //------QUESTION 1 -----//
-    //$controleur->q1();
-
-    ///------QUESTION 2 -----///
-    //$controleur->q2();
-
-    ///------QUESTION 3 -----///
-    //$controleur->q3();
-
-    ///------QUESTION 4 -----///
-    //$controleur->q4();
-
-    ///------QUESTION 5 -----///
-    //$controleur->q5();
-
-    ///------Partie 2 -----///
-    ///
-    ///------QUESTION 1 -----///
-    //$controleur->question21();
-
-    ///------QUESTION 2 -----///
-    //$controleur->question22();
-
-    ///------QUESTION 3 -----///
-    //$controleur->question23();
-
-    ///------QUESTION 4 -----///
-    //$controleur->question24();
-
-    ///------QUESTION 5 -----///
-    //$controleur->question25();
-
-    ///------QUESTION 7 -----///
-    //$controleur->question27();
-
-    ///------QUESTION 8 -----///
-    //$controleur->question28();
-
-    ///------QUESTION 9 -----///
-    //$controleur->question29();
-
-
-    ///------Partie 2 -----///
-    ///
-
-    ///------QUESTION 1 -----///
-    //$controleur->question31();
-
-    ///------QUESTION 2 -----///
-    //$controleur->question32();
-
-    ///------QUESTION 3 -----///
-    //$controleur->question33();
-
-    ///------QUESTION 4 -----///
-    //$controleur->question34();
-
-    ///------QUESTION 1 partie 2 -----///
-    $controleur->tempsExecution3();
-
 
 })->setName("Menu");
+
+
+
+
 
 $db = new DB();
 $db->addConnection(parse_ini_file("src/conf/db.config.ini"));
